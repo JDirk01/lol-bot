7fcbcdc454f84ced0bfeb0c533ff82fa9e6b3c66
JDirk01 <jarrendirk4@gmail.com>
2023-12-16 16:28:05 -0800

    t

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import os
+
+# consts, api token and guild from env vars.
+API_TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
+    
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
